aace342e291dcee1f8ba1d5ef0275033c5e6701c
Konrad von Broen <kbroen98@gmail.com>
2021-05-12 16:26:23 +0200

    Added new views

---
+++ SwiftDataAcquisition/BarChartView.swift
@@ -0,0 +1,29 @@
+//
+//  BarChartView.swift
+//  SwiftDataAcquisition
+//
+//  Created by Konrad von Broen on 12/05/2021.
+//
+
+import UIKit
+
+class BarChartView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
