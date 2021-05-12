9b97e542f95d8d14aff67156c7d36cd9d9f36e82
Konrad von Broen <kbroen98@gmail.com>
2021-05-12 15:57:46 +0200

    Init commit

---
+++ SwiftDataAcquisition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDataAcquisition
+//
+//  Created by Konrad von Broen on 12/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
